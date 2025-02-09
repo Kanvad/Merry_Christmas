a296c4216c37f998a52bb370460673508f97ddae
Kanvad <tanphat070500@gmail.com>
2025-02-09 17:47:36 +0700

    create merge_repos.sh and run it

--- merge_repos.sh
+++ merge_repos.sh
@@ -8,28 +8,13 @@ REPOS=(
     "https://github.com/Kanvad/Merry_Christmas4.git"
 )
 
-# Tạo một thư mục mới cho repo chính
-# MAIN_REPO="files"
-# mkdir $MAIN_REPO
-# cd $MAIN_REPO
-
-# Khởi tạo một repository Git mới
-git init
-
 # Lặp qua từng repo trong danh sách
 for REPO in "${REPOS[@]}"; do
-    # Lấy tên thư mục từ URL
-    REPO_NAME=$(basename "$(dirname "$REPO")")
+    # Lấy tên repo từ URL
+    REPO_NAME=$(basename "$REPO" .git)
 
     # Clone repo vào thư mục riêng
     git clone "$REPO" "$REPO_NAME"
-
 done
 
-# Thêm tất cả các tệp vào repo chính
-git add .
-
-# Commit các thay đổi
-git commit -m "Gộp các repository"
-
-echo "Đã gộp các repository thành công vào $MAIN_REPO!"
+echo "Đã gộp các repository thành công"
